7f11c58e402febba4503c52747e0d1a04ceeb55b
bigfacewo <812166094@qq.com>
2016-04-25 19:39:06 +0800

    add arrow kry control when add feature point

---
+++ ImageStitchingSystem/UI/Weight/ScrollViewerWithoutKeyDown.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageStitchingSystem.UI.Weight
+{
+    /// <summary>
+    /// ScrollViewerWithoutKeyDown.xaml 的交互逻辑
+    /// </summary>
+    public partial class ScrollViewerWithoutKeyDown : ScrollViewer
+    {
+        public ScrollViewerWithoutKeyDown()
+        {
+            InitializeComponent();
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+           // base.OnKeyDown(e);
+        }
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+           // base.OnKeyUp(e);
+        }
+
+    }
+}
